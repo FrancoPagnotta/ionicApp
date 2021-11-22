b601a11394d023e3090a4a6b7c0e8e56407b5412
FrancoPagnotta <fpagnotta@mobydigital.com>
2021-11-22 12:54:57 -0300

    se agregaron metodos para crear nuevas listas, agregar items y se implelento uso de localStorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from 'src/app/services/deseos.service';
 
@@ -9,6 +11,46 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class Tab1Page {
   
-  constructor(public _deseosService: DeseosService) {
+  constructor(public _deseosService: DeseosService, 
+              private _router: Router,
+              public _alertController: AlertController ) {
   }
+
+  async agregarLista() {
+    // this._router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this._alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Crear',
+          handler: (data) => {
+
+            console.log('Data del formulario --> ', data);
+            if (data.titulo.length === 0) return;
+            else this._deseosService.crearLista(data.titulo);
+
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+    
+  }
+  
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,13 +8,22 @@ export class DeseosService {
 
   listas: Lista[] = [];
 
-  constructor() { // Este constructor se va a llamar una unica vez aunque haga referencia a este servicio en varios componentes, o sea lo que este constructor contenga se va a ejecutar una unica vez aunque haga referencia a este servicio en varios componentes. 
-    
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-
-    this.listas.push(lista1, lista2);
-
-    console.log('Listas --> ',this.listas);
+  constructor() { // El constructor se va a llamar una unica vez cuando se haga referencia a este servicio por primera vez en un componente. Aunque llame a este servicio en varios componentes, este se dispara una unica vez, la primera vez que es llamado.
+    this.cargarStorage();
   }
+  
+  crearLista(titulo: string) {
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas)); //JSON.stringify lo que hace es convertir un objeto en un json valido de tipo string
+  }
+
+  cargarStorage(){
+    if (localStorage.getItem('data')) this.listas = JSON.parse(localStorage.getItem('data')); // Validamos que exista la data en el localStorage y si existe inicializamos this.listas con esta data, de lo contrario no hacemos nada y this.listas queda vacio porque cuando lo declaramos lo inicializamos vacio. Esta validacion la hacemos porque de lo contrario, si solo hacemos JSON.parse(localStorage.getItem('data')) y la data no existe, el JSON.parse devolveria null y eso nos daria un error.
+  } 
 }
+
